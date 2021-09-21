b0c1d3a123cee95122e56dfa960cd578e7273a81
Lucky <vagyasri2011@gmail.com>
2021-09-21 22:03:05 +0200

    Add and Check the answers

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Question 1 - What is the output of each of the console log?
 const color = 'red';
 const animal = 'dog';
@@ -5,8 +6,8 @@ const animal = 'dog';
   const color = 'blue';
   const animal = 'cat';
 
-  console.log(`log 1 - color: ${color}`);
-  console.log(`log 2 - animal: ${animal}`);
+  console.log(`log 1 - color: ${color}`); // log 1 - color: blue
+  console.log(`log 2 - animal: ${animal}`); // log 2 - animal: cat
 }
-console.log(`log 3 - color: ${color}`);
-console.log(`log 4 - animal: ${animal}`);
+console.log(`log 3 - color: ${color}`); // log 1 - color: red
+console.log(`log 4 - animal: ${animal}`); // log 1 - animal: dog
