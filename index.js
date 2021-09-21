bf059f7b3157818ec3f01c157b89c2bcfc7477b8
Lucky <vagyasri2011@gmail.com>
2021-09-21 21:58:51 +0200

    Add question 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Question 1 - What is the output of each of the console log?
+const color = 'red';
+const animal = 'dog';
+{
+  const color = 'blue';
+  const animal = 'cat';
 
+  console.log(`log 1 - color: ${color}`);
+  console.log(`log 2 - animal: ${animal}`);
+}
+console.log(`log 3 - color: ${color}`);
+console.log(`log 4 - animal: ${animal}`);
